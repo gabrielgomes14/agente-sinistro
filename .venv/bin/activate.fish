65c146aaaae721e3e9a9cb6df4b8a5a08a9128be
gabrielgomes14 <gomesgalikosky@gmail.com>
2025-08-23 01:24:58 -0300

    feature

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /mnt/c/Users/GAMER/agente-sinistro/agente-sinistro/.venv
+set -gx VIRTUAL_ENV /mnt/c/Users/GAMER/agente-ecar/agente-sinistro/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
